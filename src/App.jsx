d0cfd541a0272448ad100f30246a4bec1e97e3ff
debiabril <debora.badosa@gmail.com>
2023-11-30 14:38:13 -0300

    under construction

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex justify-center h-[100vh]'>
+        <img className='object-cover m-auto' src="https://colegiomaya.edu.sv/en/wp-content/uploads/2018/03/underconstruction.jpg" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
